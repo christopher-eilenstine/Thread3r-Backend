49fad8b96fa7898d3b31cd5bdb269aabd78f0da5
Christopher Eilenstine <ceilenstine@skuld.local>
2020-10-30 17:24:16 -0500

    inital commit

---
+++ thread3r-backend/src/main/java/com/thread3r/thread3rbackend/Thread3rBackendApplication.java
@@ -0,0 +1,13 @@
+package com.thread3r.thread3rbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Thread3rBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Thread3rBackendApplication.class, args);
+	}
+
+}
---
+++ thread3r-backend/src/test/java/com/thread3r/thread3rbackend/Thread3rBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thread3r.thread3rbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Thread3rBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
